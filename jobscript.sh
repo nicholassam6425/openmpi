104588db0a320cc5e6d01c8d5dc9612a7dd3823b
nick <nicholassam6425@gmail.com>
2023-01-18 15:45:37 -0500

    helloworld without mesasges

--- jobscript.sh
+++ jobscript.sh
@@ -5,11 +5,8 @@
 #SBATCH --job-name mpi_job
 #SBATCH --output=mpi_output_%j.txt
 #SBATCH --mail-type=FAIL
- 
 cd $SLURM_SUBMIT_DIR
- 
 module load intel
 module load openmpi
- 
 mpirun ./a.out
 # or "srun ./mpi_example"
