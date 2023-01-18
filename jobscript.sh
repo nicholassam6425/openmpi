431b2e443a6888a814e72fc0aefb4477c5a30092
nick <nicholassam6425@gmail.com>
2023-01-18 15:16:20 -0500

    Create jobscript.sh

---
+++ jobscript.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=2
+#SBATCH --time=00:02:00
+#SBATCH --job-name mpi_job
+#SBATCH --output=mpi_output_%j.txt
+#SBATCH --mail-type=FAIL
+ 
+cd $SLURM_SUBMIT_DIR
+ 
+module load intel
+module load openmpi
+ 
+mpirun ./a.out
+# or "srun ./mpi_example"
